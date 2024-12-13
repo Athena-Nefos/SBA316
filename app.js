a4557e1c83e8c68ce94f5f6933caebdeba9cf91b
Athena Nefos <athena.nefos@gmail.com>
2024-12-13 10:36:00 -0500

    questions 1-5

---
+++ app.js
@@ -0,0 +1,31 @@
+// Step 1. Define the questions and correct answers
+const quizData = [
+    {
+        question: "Which property would you use to get the next sibling of an element?",
+        options: ["nextNode", "nextSibling", "followingSibling"],
+        correctAnswer: "nextSibling"
+    },
+    {
+        question: "If element is an instance of an HTMLElement, what property could you use to get its direct parent node?",
+        options: ["element.getParent", "element.parent", "element.parentNode"],
+        correctAnswer: "element.parentNode"
+    }
+    {
+        question: "Which method would you use to add a child element to a parent element?",
+        options: ["parent.appendChild(child)", "parent.push(child)", "parent.addELement(child)"],
+        correctAnswer: "parent.appendChild(child)"
+    }
+    {
+        question: "Which method would you use to create a new element in the DOM?",
+        options: ["document.newElement()", "document.addElement()", "document.createElement()"],
+        correctAnswer: "document.createElement()"
+    }
+    {
+        question: "Which method returns a NodeList of elements with a specified class name?",
+        options: ["document.getElementByClassName()", "document.getElementByClass()", "document.querySelector()"],
+        correctAnswer: "document.getElementsByClassName"
+    }
+    {
+        question: "Which method "
+    }
+]
